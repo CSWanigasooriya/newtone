9f49b2181a8f9446c88c9b496afaf0ab20665848
NotChamath <chamathwanigasooriya@gmail.com>
2023-10-15 23:17:27 +0530

    show product options

--- src/app/pages/product/view/view.component.ts
+++ src/app/pages/product/view/view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, Subscription, switchMap } from 'rxjs';
 
 import { ActivatedRoute } from '@angular/router';
+import { Category } from './../../../models/category.model';
 import { CollectionService } from '../../../services/collection.service';
 import { Product } from '../../../models/product.model';
 
@@ -15,6 +16,7 @@ export class ViewComponent implements OnInit {
   images: string[] = [];
   product$!: Observable<Partial<Product> | undefined>;
   selectedId!: string | null | undefined;
+  category$!: Observable<Partial<Category | undefined>>;
 
   private _subscriptions = new Subscription();
 
@@ -29,12 +31,14 @@ export class ViewComponent implements OnInit {
     this.product$ = this.route.paramMap.pipe(
       switchMap((params) => {
         this.selectedId = params.get('id')?.trim();
-        console.log(this.selectedId);
-        return this._collection.getProduct(this.selectedId || '');
+        return this._collection.getProduct(this.selectedId || '0');
       })
     );
     this._subscriptions.add(
       this.product$.subscribe((product) => {
+        this.category$ = this._collection.getCategory(
+          product?.categoryId || '0'
+        );
         this.images = product?.imageURLs || [];
       })
     );
