36392ecc36589e61d7ab819de713ffe440b2ced2
NotChamath <chamathwanigasooriya@gmail.com>
2023-10-22 02:04:36 +0530

    update admin dashboard

--- src/app/pages/admin/dashboard/dashboard.component.ts
+++ src/app/pages/admin/dashboard/dashboard.component.ts
@@ -1,8 +1,15 @@
+import { CollectionService } from './../../../services/collection.service';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from './../../../models/product.model';
 
 @Component({
   selector: 'newtone-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  products$: Observable<Partial<Product>[]> = this._collection.getProducts();
+
+  constructor(private _collection: CollectionService) {}
+}
