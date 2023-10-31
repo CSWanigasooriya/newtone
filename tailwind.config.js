cb2674c7899e74f4f650fbd52f0a4a74731c491d
NotChamath <chamathwanigasooriya@gmail.com>
2023-10-31 22:11:04 +0530

    remove flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,9 @@ module.exports = {
   content: [
     join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
-    "./node_modules/flowbite/**/*.js" // add this line
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('flowbite/plugin') // add this line
-  ],
+  plugins: [],
 };
