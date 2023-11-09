700442fb91328a4d8f58d8938984d9bf8928d55d
SHCKDSilva <dilshandesilva.shc@gmail.com>
2023-11-09 14:26:28 +0530

    added about-us

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -6,6 +6,7 @@ import { LatestComponent } from './latest/latest.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../../shared/shared.module';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from '../../shared/shared.module';
     LatestComponent,
     HomeComponent,
     BannerComponent,
+    AboutUsComponent,
   ],
   imports: [CommonModule, RouterModule, SharedModule],
 })
